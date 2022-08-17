ae4210345dfeb3a8d2640ec49a45322820240a42
Crayen <transparent_legacy@yahoo.com>
2022-08-17 14:52:37 -0500

    final issue

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -98,5 +98,19 @@ var displayRepos = function(repos, searchTerm) {
   }
 };
 
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  
+    fetch(apiUrl).then(function(response) {
+      if (response.ok) {
+        response.json().then(function(data) {
+          displayRepos(data.items, language);
+        });
+      } else {
+        alert('Error: GitHub User Not Found');
+      }
+    });
+  };
 
+  languageButtonsEl.addEventListener("click", buttonClickHandler);
 userFormEl.addEventListener("submit", formSubmitHandler);
